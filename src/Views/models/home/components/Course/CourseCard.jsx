cfb7b6008c70f073bbcae2101a9701ad5dd3bd0f
graytrann <trankhaiphong987@gmail.com>
2024-04-05 10:35:37 +0700

    edit Course logic

--- src/Views/models/home/components/Course/CourseCard.jsx
+++ src/Views/models/home/components/Course/CourseCard.jsx
@@ -1,56 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { Container, Divider } from "@mui/material";
-import {
-  getAllCourse,
-  getAscCourse,
-  getCourseById,
-  getDesCourse,
-} from "../../../../../Controllers/course";
+import React from "react";
+import { Divider } from "@mui/material";
+
 import { FcAlarmClock } from "react-icons/fc";
 import { IoEyeSharp } from "react-icons/io5";
 import { BellOutlined } from "@ant-design/icons";
 import { NavLink } from "react-router-dom";
-const CourseCard = () => {
-  const [courses, setCourses] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const coursesData = await getAllCourse();
-        setCourses(coursesData);
-        const id = 1;
-        const course = await getCourseById(id);
-      } catch (error) {
-        console.error("Failed to fetch courses:", error);
-      }
-    }
-
-    fetchData();
-  }, []);
-
-  const fetchDesCourse = async () => {
-    try {
-      const data = await getDesCourse();
-      setCourses(data);
-    } catch (error) {
-      console.error("Failed to fetch courses:", error);
-    }
-  };
-  const fetchAscCourse = async () => {
-    try {
-      const data = await getAscCourse();
-      setCourses(data);
-    } catch (error) {
-      console.error("Failed to fetch courses:", error);
-    }
-  };
+const CourseCard = ({ courses }) => {
   return (
     <>
-      <button onClick={fetchDesCourse}>Sắp xếp giảm dần</button>
-      <button onClick={fetchAscCourse}>Sắp xếp tăng dần</button>
-
       {courses.map((course, index) => (
-        <div key={index} className="duration-300 shadow-md hover:shadow-xl group">
-          <NavLink to={`course/get-course/${course.ID}`} activeClassName="active">
+        <div
+          key={index}
+          className="duration-300 shadow-md hover:shadow-xl group"
+        >
+          <NavLink
+            to={`course/get-course/${course.ID}`}
+            activeClassName="active"
+          >
             <div className="relative">
               <div className="overflow-hidden">
                 <img
@@ -59,16 +25,6 @@ const CourseCard = () => {
                   className="object-cover w-full aspect-[3/2] group-hover:scale-110 duration-300"
                 />
               </div>
-              {/* {isPopular ? (
-                <span
-                  id="like__tag--home"
-                  className="absolute top-0 -left-2 px-3 py-1 text-white bg-red-600 translate-y-[50%] shadow-[6px_5px_10px_black]"
-                >
-                  Yêu thích
-                </span>
-              ) : (
-                <></>
-              )} */}
 
               <span className="`absolute mr-80 bottom-0 left-0 px-3 py-1 bg-teal-500 text-white translate-y-[50%] shadow-lg ${}`">
                 {course.NameCourse}
@@ -76,24 +32,13 @@ const CourseCard = () => {
             </div>
             <div className="p-2 font-medium duration-500 line-clamp-2 hover:text-orange-400">
               {course.Description}
-              {/* <NavLink
-                to={`/detail/${maKhoaHoc}`}
-                className="font-medium duration-500 line-clamp-2 hover:text-orange-400"
-                onClick={() => {
-                  dispatch(setNavbarActive(false));
-                }}
-              >
-                {moTa + tempContent}
-              </NavLink> */}
+
               <div className="flex flex-wrap items-center justify-between pt-3 text-gray-500">
                 <p>
                   <FcAlarmClock className="text-red-400 me-2" />
                   <>3 giờ</>
                 </p>
-                <p>
-                  {/* <MdCalendarMonth className="text-orange-400 me-2" />
-                  <>12 tuần</> */}
-                </p>
+                <p></p>
                 <p>
                   <IoEyeSharp className="text-blue-500 me-2" />
                   <>{course.Goal}</>
@@ -103,8 +48,14 @@ const CourseCard = () => {
             <Divider className="m-0" />
             <div className="flex flex-wrap items-center justify-between px-5 py-3">
               <div className="flex items-center">
-                <img src="./home_carousel_03.jpg" alt="User" className="object-cover w-10 me-2" />
-                <span className="text-xs text-gray-500 sm:text-base">TrUnG</span>
+                <img
+                  src="./home_carousel_03.jpg"
+                  alt="User"
+                  className="object-cover w-10 me-2"
+                />
+                <span className="text-xs text-gray-500 sm:text-base">
+                  TrUnG
+                </span>
               </div>
               <div className="text-right">
                 <div className="flex items-center">
--- src/Views/models/home/components/Course/Courses.jsx
+++ src/Views/models/home/components/Course/Courses.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CourseCard from "./CourseCard";
 import { Container } from "@mui/material";
 import { CaretRightOutlined } from "@ant-design/icons";
+import {
+  getAllCourse,
+  getAscCourse,
+  getDesCourse,
+} from "../../../../../Controllers/course";
 export default function Courses() {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const coursesData = await getAllCourse();
+        setCourses(coursesData);
+      } catch (error) {
+        console.error("Failed to fetch courses:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const fetchDesCourse = async () => {
+    try {
+      const data = await getDesCourse();
+      setCourses(data);
+    } catch (error) {
+      console.error("Failed to fetch courses:", error);
+    }
+  };
+  const fetchAscCourse = async () => {
+    try {
+      const data = await getAscCourse();
+      setCourses(data);
+    } catch (error) {
+      console.error("Failed to fetch courses:", error);
+    }
+  };
   return (
     <>
       <div className="py-10 text-white bg-orange-400">
         <div className="px-4 mx-auto max-w-screen-2xl">
-          <h1 className="mr-[81%] text-4xl font-bold uppercase sm:text-5xl">KHÓA HỌC</h1>
+          <h1 className="mr-[81%] text-4xl font-bold uppercase sm:text-5xl">
+            KHÓA HỌC
+          </h1>
           <p className="flex items-center text-black sm:text-lg">
             <CaretRightOutlined className="me-1 animate-pulse" />
             Bắt đầu hành trình nào!
@@ -15,8 +52,12 @@ export default function Courses() {
         </div>
       </div>
       <Container>
+        <div>
+          <button onClick={fetchAscCourse}>TĂNG</button>
+          <button onClick={fetchDesCourse}>GIẢM</button>
+        </div>
         <div className="grid gap-10 px-4 mt-8 mb-8 xl:px-0 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4">
-          <CourseCard />
+          <CourseCard courses={courses} />
         </div>
       </Container>
     </>
