f3ebdf93370dc6d2e8d4926b30aec10fbfc05ed3
graytrann <trankhaiphong987@gmail.com>
2024-04-08 14:54:56 +0700

    last push

--- src/Controllers/fetcher.js
+++ src/Controllers/fetcher.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Setup axios instance - tạo ra cấu hình mặc định
 const fetcher = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://gym-nodejs.onrender.com",
 });
 
 export default fetcher;
